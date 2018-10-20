54b8800a1e8b4a75bbfc81ab32f6a2c10eee13e9
Bless <norbertbodziony@gmail.com>
2018-10-20 13:22:58 +0200

    Initial commit

---
+++ KNN.py
@@ -0,0 +1,15 @@
+import sys
+import numpy
+import scipy
+import pandas
+
+class knn :
+    def __init__(self,K,learning_data:list):
+       self.K=K
+       self.learning_data=learning_data
+
+    def predict(self,test_data:list):
+        pass
+
+    def score(self,test_data:list):
+        pass
